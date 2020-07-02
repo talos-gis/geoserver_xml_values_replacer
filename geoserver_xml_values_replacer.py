4c071c99f3a4599fde9e8093a8a5915937eb501d
Idan Miara <idan@miara.com>
2020-07-02 18:51:58 +0300

    now it works

--- geoserver_xml_values_replacer.py
+++ geoserver_xml_values_replacer.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import re
 
@@ -6,38 +7,47 @@ replace tile size (or other parameter) in Geoserver xml files
 '''
 
 
-def change_pattern_in_file(filename, key, pattern, new_value):
+def change_pattern_in_file(filename, key, pattern, new_value, make_backup=True):
     # I could use proper read/write xml, but it seems like an overkill
     print(filename)
     filename = Path(filename).resolve()
     if not filename.exists():
         raise Exception('filename {} does not exist'.format(filename))
-    my_file = open(str(filename), "r")
-    lines_of_file = my_file.readlines()
-    my_line_i = None
-    for i, line in enumerate(lines_of_file):
+    lines = open(str(filename), "r").readlines()
+    line_i = None
+    for i, line in enumerate(lines):
         stripped = line.strip()
         if stripped == key:
-            my_line_i = i+1
+            line_i = i+1
             break
-    if my_line_i:
-        my_line = lines_of_file[my_line_i]
-        new_line = pattern.sub(new_value, my_line)
-        lines_of_file[my_line_i] = new_line
-        if new_line == new_line:
+    if line_i:
+        old_line = lines[line_i]
+        new_line = pattern.sub(new_value, old_line)
+        if old_line == new_line:
             print('pattern was not replaced!')
             return False
+        lines[line_i] = new_line
     else:
         print('key not found')
         return False
-    my_file = open(str(filename), "w")
-    my_file.writelines(lines_of_file)
+    if make_backup:
+        backup_name = filename.with_suffix('.bak')
+        if backup_name.exists():
+            print('backup file exists {}'.format(backup_name))
+        else:
+            os.rename(str(filename), str(backup_name))
+    open(str(filename), "w").writelines(lines)
     return True
 
 
 def change_pattern_glob(root: Path, file_pattern: str, **kwargs):
+    result = 0
+    total = 0
     for filename in root.glob(file_pattern):
-        change_pattern_in_file(filename=filename, **kwargs)
+        total += 1
+        if change_pattern_in_file(filename=filename, **kwargs):
+            result += 1
+    return total, result
 
 
 # <coverage>
@@ -62,8 +72,12 @@ def geoserver_tilesize_change(data_dir, workspace):
     new_value = pattern.format('256,256')
     root = Path(data_dir) / 'workspaces' / workspace
     file_pattern = '**/**/coverage.xml'
+    make_backup = True
 
-    change_pattern_glob(root=root, file_pattern=file_pattern, key=key, pattern=p, new_value=new_value)
+    total, success = change_pattern_glob(
+        root=root, file_pattern=file_pattern,
+        key=key, pattern=p, new_value=new_value, make_backup=make_backup)
+    print('files processed {}/{}'.format(success, total))
 
 
 if __name__ == '__main__':
